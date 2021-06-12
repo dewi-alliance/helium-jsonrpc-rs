c982eb66a9405881737a36997960b6a1304d927b
Chris Bruce <chris@lumeo.com>
2021-06-12 13:16:30 +0200

    refactor client post into asyn fn

--- examples/get_block.rs
+++ examples/get_block.rs
@@ -2,7 +2,6 @@ use helium_jsonrpc::{blocks, Client};
 
 #[tokio::main]
 async fn main() {
-    
     let client = Client::new_with_base_url("http://localhost:4467".to_string());
     let height = blocks::height(&client).await.unwrap();
 
