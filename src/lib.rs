90f592d74df602caf404e1811d582d6bd09af31c
Louis Thiery <thiery.louis@gmail.com>
2021-08-14 10:23:57 -0400

    remove debug print

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,6 @@ impl Client {
         let request = self.client.post(&request_url).json(&data);
         let response = request.send().await?;
         let body = response.text().await?;
-        println!("{}", body);
         let v: Response<T> = serde_json::from_str(&body)?;
         match v {
             Response::Data { result, .. } => Ok(result),
